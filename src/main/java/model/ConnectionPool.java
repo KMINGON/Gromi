4c6196366c7f9ead63cc7a7ba661494c476ef2c4
KMINGON <rlaalsrhs59@gmail.com>
2023-05-31 20:43:35 +0900

    커넥션 및 user 클래스 작성

--- src/main/java/model/ConnectionPool.java
+++ src/main/java/model/ConnectionPool.java
@@ -1,5 +1,24 @@
 package model;
 
+import java.sql.Connection;
+
 public class ConnectionPool {
 
+    private static ConnectionPool instance;
+    private Connection connection;
+
+    private ConnectionPool() {
+        this.connection = new ConnectMysql().getConn();
+    }
+
+    public static ConnectionPool getInstance() {
+        if (instance == null) {
+            instance = new ConnectionPool();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
